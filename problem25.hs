240797ec66b1d85a4419e45f44e8eb5cbf7265a5
Сло <elephantum@zoo.yandex.ru>
2007-01-10 13:23:31 +0300

    25 done

--- problem25.hs
+++ problem25.hs
@@ -1,4 +1,12 @@
-fib a b = (a+b):fib b (a+b)
-log10 n = log n / log 10
+add a [] = a
+add [] b = b
+add aa@(a:at) bb@(b:bt) = if ab < 10 then ab:(add at bt)
+                                     else (ab `mod` 10):(add at (add bt [1]))
+    where ab = a+b
 
-main = putStrLn $ show $ head $ filter (\x->log10 x > 2) $ fib 1 1
+fib = [1]: [1]: fib' [1] [1]
+    where fib' a b = (ab):fib' b (ab)
+              where ab = a `add` b
+
+main = print $ fst $ head $ dropWhile ((1000 >) . length . snd) $ zip [1..] fib
+
