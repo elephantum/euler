01f022bf5cce782befae175e831bfee6b30fde84
Сло <elephantum@monkeyBook.local>
2010-01-22 18:29:09 +0300

    31 done

---
+++ problem31.py
@@ -0,0 +1,17 @@
+vals = [2, 5, 10, 20, 50, 100, 200]
+
+def generate_divs(n):
+    def gen(n, left_vals):
+        if len(left_vals) == 0:
+            print
+            return 1
+
+        res = 0
+        for i in range(n/left_vals[0] + 1):
+            res += gen(n - left_vals[0]*i, left_vals[1:])
+
+        return res
+
+    return gen(n, vals)
+
+print generate_divs(200)
