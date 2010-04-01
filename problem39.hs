5dc57e7ca0bd66657d053d87f98b9c717228ce28
Сло <elephantum@monkeyBook.local>
2010-04-01 11:41:03 +0400

    39 done

---
+++ problem39.hs
@@ -0,0 +1,17 @@
+import Data.List
+import Control.Parallel
+import Control.Parallel.Strategies
+
+isRightTriangle (a, b, c) = a*a + b*b == c*c
+
+sides p = [(a, b, p - a - b) | a <- [1 .. p `div` 2], b <- [a+1 .. p-a-1], 
+           a < p-a-b, b < p-a-b]
+
+rightTriangles p = filter isRightTriangle $ sides p
+
+triangles :: [(Int, Int)]
+triangles = map (\p -> (p, length $ rightTriangles p)) [1..1000] `using` parListChunk 100 rdeepseq
+
+res = seq triangles (fst $ maximumBy (\a b -> compare (snd a) (snd b)) $ triangles)
+
+main = putStrLn $ show res
