fdf3bf99547e3e32a3c2788ba015a10bbabeb23e
Andrey Tatarinov <elephantropy@gmail.com>
2015-06-15 18:48:09 +0300

    1st attempt on primes generator in Go

---
+++ primes.go
@@ -0,0 +1,33 @@
+package primes
+
+import (
+	"fmt"
+)
+
+var prime_numbers []int = []int{}
+
+func IterPrimes() chan int {
+	res := make(chan int)
+
+	go func() {
+		for i := 2; true; i++ {
+			is_prime := true
+			for _,j := range(prime_numbers) {
+				if i == j {
+					break
+				}
+				if i % j == 0 {
+					is_prime = false
+					break
+				}
+			}
+
+			if is_prime {
+				prime_numbers = append(prime_numbers, i)
+				res <- i
+			}
+		}
+	}()
+
+	return res
+}
